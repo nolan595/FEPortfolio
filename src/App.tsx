e06062ce9fdd724e12dde9e3fde5be316efa7da9
nolan595 <98029345+nolan595@users.noreply.github.com>
2025-08-07 20:59:18 +0100

    project init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="bg-black text-white min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">🚀 Tailwind is Working!</h1>
+    </div>
+  );
+}
+
+export default App;
